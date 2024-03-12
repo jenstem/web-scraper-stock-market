ac3b0b12ba8b8234aeedb00f8f059acb7af8ed9e
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-12 15:50:00 -0400

    Find stock data and append to list

--- main.py
+++ main.py
@@ -21,3 +21,12 @@ for i in headers:
     titles.append(title)
 
 df = pd.DataFrame(columns = titles)
+
+# Data of the table
+rows = table.find_all("tr")
+
+for i in rows[1:]:
+    data = i.find_all("td")
+    row = [tr.text for tr in data]
+    l = len(df)
+    df.loc[l] = row
