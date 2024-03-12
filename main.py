aadae04c383e7e009cbf0cadf0c646e15797a79c
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-12 15:48:47 -0400

    Find titles and append to list

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ soup = BeautifulSoup(r.text, "html.parser")
 
 # Test if website is accessible or not by uncommenting first
 # print(r)
+
+# Titles of the table
+table = soup.find("table",class_ = "table table-sm table-hover screenertable")
+headers = table.find_all("th")
+
+titles = []
+
+for i in headers:
+    title = i.text
+    titles.append(title)
+
+df = pd.DataFrame(columns = titles)
